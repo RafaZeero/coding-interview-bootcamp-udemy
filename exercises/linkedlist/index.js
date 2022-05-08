b0547a5515ce788638b783b6d5746832eb351ac0
rafazeero <rafaa99@gmail.com>
2022-05-07 22:56:27 -0300

    LinkedList - Last Removed!!

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -46,6 +46,19 @@ class LinkedList {
   removeFirst() {
     return (this.head = this.head.next)
   }
+
+  removeLast() {
+    if (!this.head) return
+    if (!this.head.next) return (this.head = null)
+
+    let prevNode = this.head
+    let node = this.head.next
+    while (node.next) {
+      prevNode = node
+      node = node.next
+    }
+    prevNode.next = null
+  }
 }
 
 module.exports = { Node, LinkedList }
