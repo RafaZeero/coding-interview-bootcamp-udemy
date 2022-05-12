556a2d98cfc7783fe3efc2e658b371c6b05066a3
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-11 22:13:46 -0300

    Sorting - BubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,22 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tempArr = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tempArr
+      }
+    }
+  }
+  return arr
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
+function merge(left, right) {}
 
-}
-
-function merge(left, right) {
-
-}
-
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge }
