69f6d4a5a4e017e9f05673c889c1ec75adf6bcb4
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-11 22:35:48 -0300

    Sorting - SelectionSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -15,7 +15,24 @@ function bubbleSort(arr) {
   return arr
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i
+
+    for (let j = 1 + i; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
+
+    if (min !== i) {
+      let temp = arr[min]
+      arr[min] = arr[i]
+      arr[i] = temp
+    }
+  }
+  return arr
+}
 
 function mergeSort(arr) {}
 
